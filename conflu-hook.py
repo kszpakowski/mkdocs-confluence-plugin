e6535319ecb0ea8a9bdaa713b3d0cfc07d3f96a5
Karol Szpakowski <karol.szpakowski@gmail.com>
2023-11-27 21:29:51 +0100

    page and section updating

--- conflu-hook.py
+++ conflu-hook.py
@@ -14,53 +14,10 @@ baseUrl = "http://localhost:8090/rest/api/content"
 
 confluence_mistune = mistune.Markdown(renderer=ConfluenceRenderer(use_xhtml=True))
 
-def get_page_id(page):
-    print(f"Getting id for page: {page}")
-    url = f"{baseUrl}?title={page}"
-    r = session.get(url)
-    r.raise_for_status()
-    results = r.json()["results"]
-    return results[0]["id"] if len(results) > 0 else None
+# TODO track created or updated pages, to remove the rest of pages
+existing_pages=[]
 
-def create_content(space, title, body, parent):
-    print(f"Creating content: {title} in space {space}. Parent: {parent}")
-    url = f"{baseUrl}"
-    json = {'title': title, 
-            'type': 'page',
-            'space': {'key': space},
-            'body': body
-            }
-    if parent:
-        parent_page_id = get_page_id(parent)
-        json["ancestors"] = [{"id": parent_page_id}]
-
-    r = session.post(url, json=json)
-    r.raise_for_status()
-    return r.json()["id"]
-
-## TODO pass page content as html
-def create_page(space, title, html, parent=None):
-    body =  {
-                'storage': {
-                    'value': html,
-                    'representation': 'storage'
-                    }
-                }
-    return create_content(space, title, body, parent)
-
-
-def create_section(space, title, parent=None):
-    body = {
-                'wiki': {
-                    'value': '{pageTree:root=@self}',
-                    'representation': 'wiki'
-                    }
-                }
-    return create_content(space, title, body, parent)
-
-
-## TODO add validation if all pages and sections have unique titles
-## TODO add opt in unique title postprocessor
+# plugin handlers
 
 def on_nav(nav, config, files):
     print('--on nav')
@@ -70,9 +27,13 @@ def on_nav(nav, config, files):
 def on_page_markdown(markdown,page,config,files):
     print("--on_page_markdown")
     content = confluence_mistune(markdown)
-    create_page(space, page.title, content, page.parent.title if page.parent else None)
+    upsert_page(space, page.title, content, page.parent.title if page.parent else None)
     return markdown
 
+def on_post_build(config):
+    print(f'Now we should delete old pages. Existing pages are {existing_pages}')
+
+# ...
 
 def handleNav(nav):
     if nav.is_section:
@@ -80,6 +41,111 @@ def handleNav(nav):
 
 def handleSection(s):
     print(f'Section title: {s.title}{ f", parent: {s.parent.title}" if s.parent else ""}')
-    create_section(space, s.title,s.parent.title if s.parent else None)
+    create_or_update_section(space, s.title,s.parent.title if s.parent else None)
     for n in s.children:
         handleNav(n)
+
+# Conflu api
+
+
+# find page by it's title
+def get_page(title):
+    print(f"Getting id for page: {title}")
+    url = f"{baseUrl}?title={title}&expand=version,ancestors"
+    r = session.get(url)
+    r.raise_for_status()
+    results = r.json()["results"]
+
+    if(len(results) <= 0):
+        return None
+
+    page = {
+        'id': results[0]['id'],
+        'version': results[0]['version']['number'],
+        'ancestor': next(iter(results[0]['ancestors']), None),
+        'title': results[0]['title']
+    }
+    return page
+
+# Create page or space
+def create_content(space, title, body, parent):
+    print(f"Creating content: {title} in space {space}. Parent: {parent}")
+    url = f"{baseUrl}"
+    json = {'title': title, 
+            'type': 'page',
+            'space': {'key': space},
+            'body': body
+            }
+    if parent:
+        parent_page_id = get_page(parent)["id"]
+        json["ancestors"] = [{"id": parent_page_id}]
+
+    r = session.post(url, json=json)
+    r.raise_for_status()
+    id = r.json()["id"]
+    existing_pages.append(id)
+    return id
+
+# Update page or space
+def update_content(space, page, body, parent):
+    print(f"Updating content: {page['title']} in space {space}. Parent: {parent}")
+    url = f"{baseUrl}/{page['id']}"
+
+    json = {
+        "id": int(page['id']),
+        "status": "current",
+        "title": page['title'],
+        "type": "page",
+        "body":body,
+        'space': {'key': space},
+        "version": {
+            "number": page['version'] + 1
+        }
+    }
+
+    if parent:
+        parent_page_id = get_page(parent)["id"]
+        json["ancestors"] = [{"id": parent_page_id}]
+
+    r = session.put(url, json=json)
+    r.raise_for_status()
+    id = r.json()["id"]
+    existing_pages.append(id)
+    return id
+
+def upsert_page(space, title, html, parent=None):
+    body = {
+                'storage': {
+                    'value': html,
+                    'representation': 'storage'
+                }
+            }
+
+    page = get_page(title)
+    if page:
+        print(f'Page {title} already exists')
+        update_content(space,page,body,parent)
+    else:
+        return create_content(space, title, body, parent)
+
+
+def create_or_update_section(space, title, parent=None):
+    body = { 'wiki': {
+                'value': '{pageTree:root=@self}',
+                'representation': 'wiki'
+                }
+            }
+    section = get_page(title)
+    if section:
+        print(f'Section {title} already exists with id {section["id"]}')
+        update_content(space, section, body, parent)
+    else:
+        return create_content(space, title, body, parent)
+
+
+## TODO add validation if all pages and sections have unique titles
+## TODO add opt in unique title postprocessor
+
+
+
+
